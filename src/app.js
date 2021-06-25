b74201633a28491442216b524e593176466f6d27
Alejandro Labra <alejandro.labra@gmail.com>
2021-06-25 18:30:46 -0400

    [Proyecto Base] Configura proyecto base con priramide de pruebas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
 import express from 'express'
-//import dotenv from 'dotenv'
-
-/*if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
-}*/
 
 const app = express()
 
@@ -13,6 +8,5 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/test', (req, res) => {
   res.send('Hello Word')
 })
-//app.use('/api', routerBenefits)
 
 app.listen(5000)
